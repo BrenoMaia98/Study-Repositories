3ac33c88e16ead9ccec2e970c14d20f916427c60
Breno Maia <brenomaia98@gmail.com>
2021-11-01 01:29:37 -0300

    adding assets and default exports

---
+++ ReactNativeProject/src/assets/PokemonType/index.ts
@@ -0,0 +1,7 @@
+import fire from './PokemonType-fire.png';
+import flying from './PokemonType-flying.png';
+import grass from './PokemonType-grass.png';
+import poison from './PokemonType-poison.png';
+import water from './PokemonType-water.png';
+
+export {fire, flying, grass, poison, water};
