a019dfbb55b784a86b1f2979fc0e5aca6f55c5bd
Breno Maia <brenomaia98@gmail.com>
2021-11-08 16:34:44 -0300

    feat: pokemon details screen

--- ReactNativeProject/src/screens/PokemonDetails/index.tsx
+++ ReactNativeProject/src/screens/PokemonDetails/index.tsx
@@ -1,16 +1,86 @@
 import {useNavigation} from '@react-navigation/core';
-import React from 'react';
-import {Button, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button, FlatList} from 'react-native';
+import {PokemonImage} from '../../components/forScreens/HomeScreen/PokemonCard/styles';
+import PokemonService from '../../service/PokemonService';
+import {PokemonSpeciesDetailsResponse} from '../../service/PokemonTypes';
 import {store} from '../../storage/fakeContext';
-import {Container, ScreenTitle} from './styles';
+import AboutSection from './AboutSection';
+import EvolutionSection from './EvolutionSection';
+import StatsSection from './StatsSection';
+import {
+  ContainerScreen,
+  ScrollViewScreen,
+  ScreenTitle,
+  Divider,
+  TabItemText,
+  TabButtonView,
+  ImageView,
+} from './styles';
+type TabType = 'Sobre' | 'Status' | 'Evolução';
+
 const PokemonDetails = () => {
   const navigation = useNavigation();
+  // const detailsTabs:Array<{title:string}> = {}
+  const service = {pokemon: new PokemonService()};
+  const statusTabs: TabType[] = ['Sobre', 'Status', 'Evolução'];
+  const [selectedTab, setSelectedTab] = useState<TabType>('Sobre');
+  const [speciesDetails, setSpeciesDetails] =
+    useState<PokemonSpeciesDetailsResponse>({
+      base_happiness: 0,
+      capture_rate: 0,
+      flavor_text_entries: [{flavor_text: '', language: {name: ''}}],
+      growth_rate: {name: ''},
+    });
+
+  useEffect(() => {
+    service.pokemon
+      .getPokemonSpeciesDetails(store.pokemonData.specie)
+      .then(resp => {
+        setSpeciesDetails({
+          ...resp.data,
+          flavor_text_entries: resp.data.flavor_text_entries.filter(item => {
+            return item.language.name === 'en';
+          }),
+        });
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <Container>
-      <ScreenTitle>PokemonDetails</ScreenTitle>
-      <Text>{JSON.stringify(store, null, 2)}</Text>
-      <Button title="Go Back" onPress={() => navigation.goBack()} />
-    </Container>
+    <ContainerScreen>
+      <ScrollViewScreen>
+        <Button title="Go Back" onPress={() => navigation.goBack()} />
+        <ImageView>
+          <PokemonImage source={{uri: store.pokemonData.image}} />
+        </ImageView>
+        <FlatList
+          data={statusTabs}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <TabButtonView
+              onPress={() => {
+                setSelectedTab(item);
+              }}
+              type={store.pokemonData.mainType}>
+              <TabItemText
+                active={selectedTab === item}
+                type={store.pokemonData.mainType}>
+                {item}
+              </TabItemText>
+            </TabButtonView>
+          )}
+          horizontal
+        />
+        {selectedTab === 'Sobre' && <AboutSection {...speciesDetails} />}
+        {selectedTab === 'Status' && (
+          <StatsSection stats={store.pokemonData.stats} color="black" />
+        )}
+        {selectedTab === 'Evolução' && <EvolutionSection />}
+
+        <Divider />
+      </ScrollViewScreen>
+    </ContainerScreen>
   );
 };
 
--- ReactNativeProject/src/screens/PokemonDetails/styles.ts
+++ ReactNativeProject/src/screens/PokemonDetails/styles.ts
@@ -1,13 +1,57 @@
+import {TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
+import {PokemonTypes} from '../../service/PokemonTypes';
+import {normalize} from '../../themes/globalTheme/theme';
 
-export const Container = styled.View`
-  justify-content: center;
-  align-items: center;
+export const ContainerScreen = styled.SafeAreaView`
+  height: ${({theme}) => theme.utils.SCREEN_HEIGHT}px;
+  padding-bottom: ${({theme}) => theme.utils.SCREEN_HEIGHT * 0.05}px;
+`;
+
+export const ScrollViewScreen = styled.ScrollView`
   flex: 1;
+`;
+
+export const ImageView = styled.View`
+  width: ${normalize(200)}px;
+  height: ${normalize(200)}px;
+  margin: auto;
 `;
 
 export const ScreenTitle = styled.Text`
   font-size: ${({theme}) => theme.font.size.titleBigger};
-  color: ${({theme}) => theme.color.blue};
   font-weight: bold;
+`;
+
+//---------------------------------
+// Tabs
+//---------------------------------
+
+export const TabButtonView = styled(TouchableOpacity)<{
+  type: PokemonTypes;
+}>`
+  background-color: ${({theme, type}) => theme.type[type].darkColor};
+
+  margin: 0px ${normalize(5)}px;
+  padding: ${normalize(5)}px ${normalize(15)}px;
+  border-top-left-radius: ${normalize(15)}px;
+  border-top-right-radius: ${normalize(15)}px;
+`;
+
+export const TabItemText = styled.Text<{type: PokemonTypes; active: boolean}>`
+  font-size: ${({theme}) => theme.font.size.small};
+  ${({active, theme}) => active && `font-size:${theme.font.size.big}`};
+  color: ${({theme, type}) => theme.type[type].lightColorContrast};
+  font-weight: bold;
+
+  margin: auto;
+`;
+
+//---------------------------------
+// Others
+//---------------------------------
+
+export const Divider = styled.View`
+  background-color: ${({theme}) => theme.color.black};
+  height: 2px;
 `;
